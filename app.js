4ed6cedc7bfaf39910aefeb3ed355f625ab5e332
unknown <michael.amone@yahoo.com>
2021-09-05 13:00:31 +0300

    adding files

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// program to find the factorial of a number using for loop
+let number = 1;
+let factorial = 1;
+
+if( number <0){
+    console.log("Cant find factorial of negative number")
+}else{
+    for (let num = 2; num <= number; num++){
+        console.log(`${num}`);
+    }
+}
